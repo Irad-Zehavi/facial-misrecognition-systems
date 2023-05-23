8f685cf822858f3ce2b3f83c54eda3f65bd6cb97
iradz <irad.zehavi@outlook.com>
2023-05-23 14:53:03 +0000

    removed old files

--- scripts/install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-pip install pytorch_lightning==1.9.0
-pip install torchmetrics==0.10.3
-pip install -U torchvision
-pip install -U matplotlib
-pip install -U scikit-learn
-pip install -U ipympl
-pip install -U facenet_pytorch
